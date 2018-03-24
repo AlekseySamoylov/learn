c8bd27733c4a8cc8a2da11575578806714397b54
Aleksei Samoilov <alekseysamoylov89@gmail.com>
2018-03-24 15:00:38 +0300

    lombok fix

--- src/main/java/com/alekseysamoylov/learn/entity/Card.java
+++ src/main/java/com/alekseysamoylov/learn/entity/Card.java
@@ -4,8 +4,6 @@ package com.alekseysamoylov.learn.entity;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -20,45 +18,4 @@ public class Card {
     private String translation;
 
     private User cardOwner;
-
-    public String getPhrase() {
-        return phrase;
-    }
-
-    public Card setPhrase(String phrase) {
-        this.phrase = phrase;
-        return this;
-    }
-
-    public String getTranslation() {
-        return translation;
-    }
-
-    public Card setTranslation(String translation) {
-        this.translation = translation;
-        return this;
-    }
-
-    public User getCardOwner() {
-        return cardOwner;
-    }
-
-    public Card setCardOwner(User cardOwner) {
-        this.cardOwner = cardOwner;
-        return this;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Card card = (Card) o;
-        return new EqualsBuilder().append(this.phrase, card.phrase).build();
-    }
-
-    @Override
-    public int hashCode() {
-
-        return new HashCodeBuilder().append(phrase).build();
-    }
 }
--- src/main/java/com/alekseysamoylov/learn/entity/User.java
+++ src/main/java/com/alekseysamoylov/learn/entity/User.java
@@ -3,8 +3,6 @@ package com.alekseysamoylov.learn.entity;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -12,37 +10,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Accessors(chain = true)
 @Document(collection = "user")
 public class User {
-
     @Id
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public User setName(String name) {
-        this.name = name;
-        return this;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return new EqualsBuilder().append(this.name, user.name).build();
-    }
-
-    @Override
-    public int hashCode() {
-
-        return new HashCodeBuilder().append(name).build();
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                '}';
-    }
 }
